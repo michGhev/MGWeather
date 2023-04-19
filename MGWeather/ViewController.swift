1391ea7ea88b2542298b572be85983d7865103a3
Michael Ghevondyan <michael.ghevondyan@helix.am>
2023-04-19 15:01:43 +0400

    Initial Commit

---
+++ MGWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MGWeather
+//
+//  Created by Michael Ghevondyan on 19.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
